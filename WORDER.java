a6730df231d9a2eac4c69fc57a46349954fc30ff
fdgit111 <1733740970@qq.com>
2025-04-20 13:04:07 +0800

    WORDER.java

--- WORDER.java
+++ WORDER.java
@@ -20,3 +20,34 @@ public class WorkerController {
         return result;
     }
 }
+public interface WorkerMapper extends BaseMapper<Worker> {
+}
+public interface CurrentUserMapper extends BaseMapper<CurrentUser> {
+    @Select("select * from currentuser order by id desc limit 1")
+    public CurrentUser selectRecentUser();
+
+    @Select("update currentuser set password=#{password} where username=#{username}")
+    void updateByUsername(@Param("username") String username,@Param("password") String password);
+}
+@Data
+@TableName(value="worker")
+public class Worker {
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+    private String name;
+    private String phone;
+}
+@Data
+@TableName(value="student")
+public class Student {
+    @TableId(value = "stu_id", type = IdType.AUTO)
+    private Integer stuId;
+    private String stuName;
+    private String stuGender;
+    private Date stuBir;
+    private String stuPhone;
+    private String building;
+    private String roomnum;
+    private String bednumber;
+
+}
